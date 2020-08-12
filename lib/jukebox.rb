a07b96ff2f6665602150aaa23f7ce336ff0cf7f9
Noah Eakin <noaheakin@gmail.com>
2020-08-12 17:40:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,8 +23,7 @@ end
 def list (songs)
   i = 0
   while i < songs.length do
-    ordered_songs = (i+1). songs[i]
-    puts ordered_songs
+    puts songs[i] = (i+1). songs[i]
     i += 1
   end
 end
